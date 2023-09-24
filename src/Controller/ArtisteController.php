693151d56c56a57b7104807083bdb884c9aab9c1
MLB-78 <100378645+MLB-78@users.noreply.github.com>
2023-09-24 18:49:46 +0200

    Video 4

--- src/Controller/ArtisteController.php
+++ src/Controller/ArtisteController.php
@@ -2,20 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ArtisteRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArtisteController extends AbstractController
 { 
     /**
-     * @Route("/artistes", name="artistes",mathodes={Get})
+     * @Route("/artistes", name="artistes",methods={"GET"})
      */
-    public function listeArtistes(artisteRepository $repo): Response
+    public function listeArtistes(ArtisteRepository $repo)
     {
         $artistes=$repo->findAll();
         return $this->render('artiste/listeArtiste.html.twig', [
-            'lesArtistes'=> $artistes,
+            'lesArtistes'=> $artistes
         ]);
     }
 }
